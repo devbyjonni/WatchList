37e9e819f6490042605d6a0ae80fe90dbdb5a1d2
Jonni Åkesson <enterthesource@me.com>
2024-09-30 13:05:15 +0200

    Initial Commit

---
+++ WatchList/WatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchListApp.swift
+//  WatchList
+//
+//  Created by Jonni Akesson on 2024-09-30.
+//
+
+import SwiftUI
+
+@main
+struct WatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
